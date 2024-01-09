fb34988ccad95c694382cdb67e49500de9e84431
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-01-09 17:32:46 +0000

    moved the const passwordLength = getPasswordOptions(); variable

--- script.js
+++ script.js
@@ -191,10 +191,11 @@ var generateBtn = document.querySelector('#generate');
 
 // }
 
-
+console.log(password);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(passwordLength);
+  console.log(password);
   var passwordText = document.querySelector('#password');
   passwordText.value = password;
 }
